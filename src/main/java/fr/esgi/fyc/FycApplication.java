bf31ae4465d44badeda9bc0107520899a760cdfb
Bluexyi <romain-l58@hotmail.fr>
2020-10-03 20:21:24 +0200

    init MySQL container

---
+++ src/main/java/fr/esgi/fyc/FycApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.fyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/fyc/FycApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.fyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
