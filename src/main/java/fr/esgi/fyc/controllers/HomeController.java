f0fc08e47315fea336f8e3e9404a4068d5514de8
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-10-04 17:59:16 +0200

    cross origins

--- src/main/java/fr/esgi/fyc/controllers/HomeController.java
+++ src/main/java/fr/esgi/fyc/controllers/HomeController.java
@@ -1,12 +1,14 @@
 package fr.esgi.fyc.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin("*")
 public class HomeController {
 
     @GetMapping
